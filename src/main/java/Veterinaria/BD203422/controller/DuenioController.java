965289d139310b2135130b85c63136b7af904f5a
203422 <203422@ids.upchiapas.edu.mx>
2021-10-29 20:33:15 -0500

    Creacion duenio controller

---
+++ src/main/java/Veterinaria/BD203422/controller/DuenioController.java
@@ -0,0 +1,29 @@
+package Veterinaria.BD203422.controller;
+
+import Veterinaria.BD203422.model.Duenio;
+import Veterinaria.BD203422.repository.DuenioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DuenioController {
+
+    @Autowired
+    DuenioRepository duenioRepository;
+
+    @GetMapping(value = "duenio")
+    public List<Duenio> getListDuenio(){
+        return duenioRepository.findAll();
+    }
+
+    @PostMapping(value = "/Duenio/add")
+    public Duenio addDuenio(@RequestBody Duenio duenio){
+
+        return duenioRepository.save(duenio);
+    }
+}
