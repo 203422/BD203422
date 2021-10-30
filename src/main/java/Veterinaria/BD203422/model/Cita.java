0d47622c5662199aa099d0f9eee2abe2596967ac
203460 <203460@ids.upchiapas.edu.mx>
2021-10-29 20:22:31 -0500

    Creación modelo cita

--- src/main/java/Veterinaria/BD203422/model/Cita.java
+++ src/main/java/Veterinaria/BD203422/model/Cita.java
@@ -1,4 +1,65 @@
 package Veterinaria.BD203422.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cita")
 public class Cita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCita;
+
+    @Column(name = "fecha")
+    private String fecha;
+
+    @Column(name = "hora")
+    private String hora;
+
+    @Column(name = "servicio")
+    private String servicio;
+
+    public Cita(){
+
+    }
+
+    public Cita(int idCita, String fecha, String hora, String servicio){
+        this.idCita = idCita;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.servicio = servicio;
+    }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
 }
+
